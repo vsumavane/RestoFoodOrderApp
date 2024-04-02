ffd4d791ca895f9852844c65ca4ccd1e48f94b73
Vishal Umavane <vsumavane@outlook.com>
2024-04-02 06:16:35 +0530

    file added

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import classes from './header.module.css'
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  const user = {
+    name: 'John Doe',
+  };
+
+  const cart = {
+    totalCount : 10,
+  };
+  
+  const logout = () => {}
+
+
+  return (
+  <header className={classes.header}>
+        <div className={classes.container}>
+            <Link to="/" className={classes.logo}>Food Mine!</Link>
+            <nav>
+                <ul>
+                    {
+                        user? (
+                        <li className={classes.menu_container}>
+                            <Link to="/profile">{user.name}</Link>
+                        <div className={classes.menu}>
+                            <Link to="/profile">Profile</Link>
+                            <Link to="/orders">Orders</Link>
+                            <a onClick={logout}>Logout</a>
+                        </div>
+                        </li>
+                        ):(
+                        <Link to="/login">Login</Link>
+                        )}
+                        <li>
+                            <Link to="/cart">
+                                Cart {cart.totalCount>0 && <span>{cart.totalCount}</span>}</Link>
+                        </li>
+
+                </ul>
+            </nav>
+        </div>
+    </header>
+  )}
