9bf958a4bc78af5b33456e4c287a9dc0d86cb4a5
Vishal Umavane <vsumavane@outlook.com>
2024-04-03 15:56:52 +0530

    changed preoccupied port

--- backend/src/server.js
+++ backend/src/server.js
@@ -37,7 +37,7 @@ app.get('*', (req, res) => {
   res.sendFile(indexFilePath);
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => {
   console.log('listening on port ' + PORT);
 });
